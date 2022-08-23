092b29b503f79f8f664dfebe9503a2c6fc8c1f5a
Goik <sbabinski@outlook.com>
2022-08-23 01:05:10 -0300

    Develop create path method
    Fix searchMove method to correctly compare.
    Rename some methods to better clarify function.

--- KnightsTravails.js
+++ KnightsTravails.js
@@ -1,18 +1,39 @@
-const chessBoard = [];
+/*const chessBoard = [];
 for (let line = 0; line < 8; line++) {
   for (let column = 0; column < 8; column++) {
     chessBoard.push([line, column]);
   }
 }
-console.log(chessBoard);
+console.log(chessBoard);*/
 
-const knight = () => {
-  const knightMoves = (from, to) => {};
+class Play {
+  coordinate = [];
+  nextPlays = [];
 
-  const possibleMoves = (from) => {
-    const line = from[0];
-    const column = from[1];
-    const plays = [
+  constructor(coordinate) {
+    this.coordinate = coordinate;
+  }
+}
+
+class Knight {
+  knightFrom = [];
+  knightTo = [];
+  movesTree = [];
+
+  knightMoves(from, to) {
+    if (from == to) {
+      return "Error- You are already there!";
+    }
+    this.knightFrom = new Play(from);
+    this.knightTo = new Play(to);
+    this.createPath(this.knightFrom, this.knightTo);
+    console.log("yeah");
+  }
+
+  createNextPlays(play) {
+    const line = play.coordinate[0];
+    const column = play.coordinate[1];
+    const allPlays = [
       [line + 2, column + 1],
       [line + 2, column - 1],
       [line - 2, column + 1],
@@ -22,33 +43,54 @@ const knight = () => {
       [line + 1, column - 2],
       [line - 1, column - 2],
     ];
-    plays.forEach((value, index) => {
-      if (value[0] < 0 || value[1] < 0) {
-        plays.splice(index, 1);
+    const possiblePlays = allPlays.filter((value) => {
+      if (value[0] >= 0 && value[0] < 8) {
+        if (value[1] >= 0 && value[1] < 8) {
+          return true;
+        }
       }
-      if (value[0] > 7 || value[1] > 7) {
-        plays.splice(index, 1);
-      }
+      return false;
     });
-    return plays;
-  };
+    const nextPlays = possiblePlays.map((value) => {
+      return new Play(value);
+    });
+    return nextPlays;
+  }
 
-  const searchMove = (plays, to) => {
-    let correct = null;    
-    let queue = [...plays];
-    queue.forEach((value) => {
-      if (value[0] == to[0] && value[1] == to[1]) {
-        correct = value;
+  searchMove(movesTree, to) {
+    for (let index = 0; index < movesTree.length; index++) {
+      const element = movesTree[index];
+      if (
+        element.coordinate[0] == to.coordinate[0] &&
+        element.coordinate[1] == to.coordinate[1]
+      ) {
+        return true;
       }
-    });
-    if (correct) {
-      return correct;
-    } else {
-        queue.forEach((value) => {
-            return searchMove(possibleMoves(value),to);
-          });
     }
-  };
+    return false;
+  }
 
-  return { knightMoves, possibleMoves };
-};
+  createPath(play, to) {
+    play.nextPlays = this.createNextPlays(play);
+    if (this.searchMove(play.nextPlays, to)) {
+      return "found";
+    }
+    const queue = [play.nextPlays];
+    let pointer;
+    while (true) {
+      pointer = queue.shift();
+      for (let index = 0; index < pointer.length; index++) {
+        const element = pointer[index];
+        element.nextPlays = this.createNextPlays(element);
+        if (this.searchMove(element.nextPlays, to)) {
+          return console.log(element);
+        } else {
+          queue.push(element.nextPlays);
+        }
+      }
+    }
+  }
+}
+
+const knight = new Knight();
+knight.knightMoves([3, 3], [4, 3]);
